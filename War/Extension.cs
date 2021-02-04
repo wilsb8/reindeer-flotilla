8cd3d173dd35ae78884633020bfb37845f9d12ad
William A. Ferguson <WAFerguson1@student.fullsail.edu>
2021-02-03 23:16:11 -0800

    modified:   .DS_Store 	new file:   War/Extension.cs 	War/Card.cs 	War/DeckUtility.cs 	War/Player.cs 	War/Program.cs 	War/War.csproj 	War/War.sln 	War/WarApp.cs

---
+++ War/Extension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+// WAF:
+// This extension of Enqueue (Queue<T>) takes all the cards and places them in the Queue<> at once
+// instead of one at a time for simplicity. The regular Enqueue method of Queue<T> would have worked just fine but I needed
+// it to do something specific.
+//
+// Reference: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/extension-methods
+
+namespace War
+{
+    public static class Extension
+    {
+        public static void Enqueue(this Queue<Card> cards, Queue<Card> newCards)
+        {
+            foreach (var card in newCards)
+            {
+                cards.Enqueue(card);
+            }
+        }
+    }
+}
