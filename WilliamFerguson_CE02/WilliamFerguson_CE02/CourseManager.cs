3ac5343822c50b4a5a828e964f9703664776e7f2
William A. Ferguson <WAFerguson1@student.fullsail.edu>
2021-02-05 13:06:32 -0800

    Created logic for CourseManager.CreateCourse()

--- WilliamFerguson_CE02/WilliamFerguson_CE02/CourseManager.cs
+++ WilliamFerguson_CE02/WilliamFerguson_CE02/CourseManager.cs
@@ -5,8 +5,10 @@ namespace WilliamFerguson_CE02
     {
         private static int _selection; // for our menu choice
         public static bool endThisNow = false; // signal to end the program
-
-
+        private static Course _course = null;
+        private static Teacher _teacher = null;
+        private static Student[] _students = null;
+        public static int numberOfStudents = 0;
 
         public CourseManager()
         {
@@ -15,10 +17,11 @@ namespace WilliamFerguson_CE02
 
         public static void Init()
         {
+
             
-            Console.Clear(); // clear the screen
             while (!endThisNow)
             {
+                Console.Clear(); // clear the screen
                 Menu.Init(); // display our menu
                 Console.Write("\nSelection: ");
                 string choice = Console.ReadLine();
@@ -38,7 +41,7 @@ namespace WilliamFerguson_CE02
                         Display();
                         break;
                     case 5:
-                        Exit();
+                        endThisNow = Exit();
                         break;
                     default:
                         Console.WriteLine("Selection not recognized.");
@@ -52,10 +55,38 @@ namespace WilliamFerguson_CE02
 
         }
 
-        public static void CreateCourse() { }
+        public static void CreateCourse()
+        {
+            Console.Clear();
+            Console.Write("Course Name: ");
+            var courseTitleName = Console.ReadLine();
+            string ctn = Validation.MyString(courseTitleName);
+            Console.Write("Course description: ");
+            var courseDescription = Console.ReadLine();
+            string cdes = Validation.MyString(courseDescription);
+
+            _students = new Student[numberOfStudents];
+            _course = new Course(ctn, cdes, numberOfStudents);
+
+            if (_teacher != null)
+            {
+                _course.Teacher = _teacher;
+                Console.WriteLine("Course has been added with " + _teacher.Name);
+            }
+
+            Console.WriteLine("Course has been successfully created/added");
+        }
+
+
         public static void CreateTeacher() { }
         public static void AddStudents() { }
         public static void Display() { }
-        public static void Exit() { }
+
+        public static bool Exit()
+        {
+            bool isExit = true;
+            return isExit;
+
+        }
     }
 }
