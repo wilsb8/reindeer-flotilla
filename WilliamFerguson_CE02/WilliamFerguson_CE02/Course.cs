cb4dd02d599552d4e9b9efc28d222963e25180d0
William A. Ferguson <WAFerguson1@student.fullsail.edu>
2021-02-05 08:45:26 -0800

    modified:   .DS_Store 	new file:   WilliamFerguson_CE02/WilliamFerguson_CE02/Course.cs 	new file:   WilliamFerguson_CE02/WilliamFerguson_CE02/CourseManager.cs 	modified:   WilliamFerguson_CE02/WilliamFerguson_CE02/WilliamFerguson_CE02.csproj

---
+++ WilliamFerguson_CE02/WilliamFerguson_CE02/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace WilliamFerguson_CE02
+{
+    public class Course
+    {
+        public string CourseTitle { get; set; }
+        public string CourseDescription { get; set; }
+        public TTeacher Teacher { get; set; }
+        public Student[] Students { get; set; }
+
+        public Course(string _courseTitle, string _courseDescription, int _numberOfStudents)
+        {
+            CourseTitle = _courseTitle;
+            CourseDescription = _courseDescription;
+            Student = new Student[_numberOfStudents];
+        }
+    }
+}
---
+++ WilliamFerguson_CE02/WilliamFerguson_CE02/CourseManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace WilliamFerguson_CE02
+{
+    public class CourseManager
+    {
+
+        public CourseManager()
+        {
+        }
+
+        public static void CreateCourse() { }
+        public static void CreateTeacher() { }
+        public static void AddStudents() { }
+        public static void Display() { }
+        public static void Exit() { }
+    }
+}
